353f74a2ff464484e64f5b4a5d690897d15e397d
Peter Haro <peter.haro@sintef.no>
2019-05-03 00:08:37 +0200

    Forgot to commit, Going to use channel for circular buffer to do the possibility of single write, multiple readers! Cool stuff

--- Snapfish.Application/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Snapfish.Application
-{
-    public class Class1
-    {
-    }
-}
--- Snapfish.Application/SnapfishRecorder.cs
+++ Snapfish.Application/SnapfishRecorder.cs
@@ -1,7 +1,9 @@
+using System.Threading.Channels;
+
 namespace Snapfish.Application
 {
     public class SnapfishRecorder
     {
-        
+        public static Channel<byte[]> BoundedBuffer;
     }
 }
