f0eeae9aa39cab454d07a8900ea625079b64d8f9
Erlend Stav <erlend.stav@gmail.com>
2019-05-14 17:13:24 +0200

    Added time zone to dummy data

--- Snapfish.API/Data/DbInitializer.cs
+++ Snapfish.API/Data/DbInitializer.cs
@@ -19,8 +19,8 @@ namespace Snapfish.API.Data
 
             var echograms = new EchogramInfo[]
             {
-                new EchogramInfo{ID=1, UserID=1, Latitude="632448", Longitude="102433", Source="EK80", EchogramUrl="https://www.sintef.no", Timestamp=DateTime.Parse("2019-05-12 08:01"), Biomass=""},
-                new EchogramInfo{ID=2, UserID=1, Latitude="632451", Longitude="102114", Source="EK80", EchogramUrl="https://www.nrk.no", Timestamp=DateTime.Parse("2019-05-12 09:21"), Biomass=""}
+                new EchogramInfo{ID=1, UserID=1, Latitude="632448", Longitude="102433", Source="EK80", EchogramUrl="https://www.sintef.no", Timestamp=DateTime.Parse("2019-05-12 08:01+0100"), Biomass=""},
+                new EchogramInfo{ID=2, UserID=1, Latitude="632451", Longitude="102114", Source="EK80", EchogramUrl="https://www.nrk.no", Timestamp=DateTime.Parse("2019-05-12 09:21+0100"), Biomass=""}
             };
             foreach (EchogramInfo e in echograms)
             {
@@ -31,8 +31,8 @@ namespace Snapfish.API.Data
 
             var snaps = new SnapMessage[]
             {
-                new SnapMessage{ID=1, EchogramInfoID = 1, Title="Mye fisk her", Comment="Vi har ikke plass til mer", Sender="Per", Receivers="Ola", SendTimestamp=DateTime.Parse("2019-05-12 08:04"), SharePublicly=false},
-                new SnapMessage{ID=2, EchogramInfoID = 2, Title="Torsk", Comment="Her blir det fangst!", Sender="Jens", Receivers="Ola", SendTimestamp=DateTime.Parse("2019-05-12 09:32"), SharePublicly=true}
+                new SnapMessage{ID=1, EchogramInfoID = 1, Title="Mye fisk her", Comment="Vi har ikke plass til mer", Sender="Per", Receivers="Ola", SendTimestamp=DateTime.Parse("2019-05-12 08:04+0100"), SharePublicly=false},
+                new SnapMessage{ID=2, EchogramInfoID = 2, Title="Torsk", Comment="Her blir det fangst!", Sender="Jens", Receivers="Ola", SendTimestamp=DateTime.Parse("2019-05-12 09:32+0100"), SharePublicly=true}
             };
             foreach (SnapMessage s in snaps)
             {
