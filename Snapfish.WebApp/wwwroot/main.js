a8229703f20393932e741e795b46fecc89b4b6fe
Erlend Stav <erlend.stav@gmail.com>
2019-09-05 14:09:01 +0200

    Updates to use of connection and API urls
    EK deamon now defaults to IP address of localhost, echogram viewer now uses the  URL of the web page to determine URL of API calls instead of hardcoded addresses

--- Snapfish.WebApp/wwwroot/main.js
+++ Snapfish.WebApp/wwwroot/main.js
@@ -1,6 +1,10 @@
 // const dataContainer = new DataContainer('http://localhost:5000/api/Snap/1');
-const dataContainer = new DataContainer('/snap.json');
-
+//const dataContainer = new DataContainer('/snap.json');
+//const dataContainer = new DataContainer('https://10.218.157.107:5003/api/snap/' + snapId);
+// In current test environment, port 5001 is used locally while 5003 is used for external calls (e.g. from tablet / phone)
+const dataContainer = (window.location.hostname.includes('localhost')) ?
+    new DataContainer('https://' + window.location.hostname + ':5001/api/snap/' + snapId) :
+    new DataContainer('https://' + window.location.hostname + ':5003/api/snap/' + snapId);
 
 const svPlot = new Plot({
     domId: 'snap',
